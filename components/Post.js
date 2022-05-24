7eec9cce74294d94917d21aaaa6657ed8b324bb1
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-23 19:14:33 -0700

    add: dummy data and map post to posts

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import Image from "next/image"
 
-function Post() {
+function Post({id, img, caption}) {
   return (
-    <div>
-      <p>1 Post</p>
+    <div className="flex flex-col items-center">
+      <div className='flex flex-col relative items-center w-96 h-96 mx-10 my-10' >
+        <Image  
+          src={img} 
+          id={id}
+          alt={id}
+          layout="fill"
+          />
+      </div>
+      <p>{caption}</p>
+
     </div>
   )
 }
--- components/Posts.js
+++ components/Posts.js
@@ -1,12 +1,28 @@
 import Post from './Post'
 
+// Connect to db, map post
+
+// dummy data
+const posts = [
+  {
+    id: 123,
+    img: '/../public/images/IMG_1144.jpg',
+    caption: 'test caption',
+  }
+]
+
+
 function Posts() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map((post) => 
+        <Post 
+          id={post.id}
+          key={post.id}
+          img={post.img}
+          caption={post.caption}
+          />
+      )}
     </div>
   )
 }
