3518bd54179a2aa495dc5ea09c7e779d09fbc8d2
CHRIS_LEE <chrismlee87@gmail.com>
2022-05-23 17:03:45 -0700

    add: feed, posts and post components

---
+++ components/Feed.js
@@ -0,0 +1,11 @@
+import Posts from './Posts'
+
+function Feed() {
+  return (
+    <div>
+      <Posts />
+    </div>
+  )
+}
+
+export default Feed
---
+++ components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+      <p>1 Post</p>
+    </div>
+  )
+}
+
+export default Post
